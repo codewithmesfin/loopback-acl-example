112c74edbb568d44fa83c89f82beb823f0c24d03
sciemesfin55 <sciemesfin55@gmail.com>
2020-08-29 00:34:35 +0300

    example of remote hook

--- common/models/user.js
+++ common/models/user.js
@@ -3,6 +3,21 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-module.exports = function(User) {
+module.exports = function (User) {
+    // User.beforeRemote("", function (ctx, next) {
+    //     let a = 2;
+    //     let b = 3;
+    // to read data from frontend UI user input
+    //use context of request.
+    //let name=ctx.body.req.firstName
+
+    // send callback back to UI or user
+    // res.send("dfgui")
+
+
+
+    //     console.log("Try to create user ....")
+    //     next();
+    // });
 
 };
